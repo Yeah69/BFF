d82d641ad7487daed73f242bdf0315c03c69e451
Dima Enns <dieter.enns@gmail.com>
2015-08-06 23:22:02 +0200

    Initial setting

---
+++ BFF/WPFStuff/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BFF.WPFStuff
+{
+    public abstract class ViewModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
