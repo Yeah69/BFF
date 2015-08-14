30f6d519daddcc871dcee55d86a87aa636b90673
Dima Enns <dieter.enns@gmail.com>
2015-08-14 18:12:34 +0200

    Adding MongoDb C# Driver and trying to connect.

---
+++ BFF/Helper/Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BFF.Helper
+{
+    class Output
+    {
+        public static void WriteLine(string text, [CallerMemberName] string callerName = null)
+        {
+            Console.WriteLine("{0}: {1}", callerName, text);
+        }
+    }
+}
---
+++ BFF/MongoDb/MongoDbHelper.cs
@@ -0,0 +1,20 @@
+﻿using BFF.Helper;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BFF.MongoDb
+{
+    public class MongoDbHelper
+    {
+        public static void testMongoDb()
+        {
+            Output.WriteLine("Connect...");
+            IMongoClient client = new MongoClient("mongodb://localhost");
+            IMongoDatabase database = client.GetDatabase("BFF");
+            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("Catagories");
+
+
+
+        }
+    }
+}
