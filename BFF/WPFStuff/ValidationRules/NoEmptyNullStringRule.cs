cdad4cb94b4b92ea69fcf77fea8881d68b7cf2bd
Dima Enns <dieter.enns@gmail.com>
2015-11-09 00:23:42 +0100

    New Account only createable when no validation errors occure and the Name is not empty.

---
+++ BFF/WPFStuff/ValidationRules/NoEmptyNullStringRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BFF.WPFStuff.ValidationRules
+{
+    class NoEmptyNullStringRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            bool validate = !string.IsNullOrEmpty((string) value);
+            return new ValidationResult(validate, validate ? null : "The text may not be empty!"); 
+            // The "Invalid"-Message is only relevant if validate is false
+        }
+    }
+}
