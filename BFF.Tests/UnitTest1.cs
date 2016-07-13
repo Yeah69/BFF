7b21d99a4eb49e07086c8522827d2f392e3421e2
Dima Enns <dieter.enns@gmail.com>
2016-07-13 09:39:51 +0200

    Introducing xUnit to the project.
    I chose xUnit as unit testing framework.
    
    Signed-off-by: Dima <dieter.enns@gmail.com>

---
+++ BFF.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BFF.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.False(false);
+        }
+    }
+}
