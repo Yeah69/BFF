7c9c9294c3c9d11cde199cf961cacc681158d3df
Dima Enns <dieter.enns@gmail.com>
2016-08-18 08:17:22 +0200

    Two Bugfixes: SummaryAccount and Flush-Buttons of Tit Creation were not displayed.
    1. The Binding Path was not refactored on the naming switch from "AllAccounts" to "SummaryAccount".
    2. Somehow the ApplyCommand-Attribute of the TitDataGrid usage got lost.
    
    Signed-off-by: Dima <dieter.enns@gmail.com>

--- BFF/MVVM/Views/TitDataGrid.xaml.cs
+++ BFF/MVVM/Views/TitDataGrid.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using BFF.MVVM.Models.Native;
-using BFF.MVVM.ViewModels.ForModels;
 using BFF.MVVM.ViewModels.ForModels.Structure;
 
 namespace BFF.MVVM.Views
