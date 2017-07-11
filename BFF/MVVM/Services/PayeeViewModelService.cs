de138aee16e95ee1054060f7e9ce26be9e29aa52
Dima Enns <dieter.enns@gmail.com>
2017-07-11 23:53:18 +0200

    Added a service class for Payees
    Signed-off-by: Dima Enns <dieter.enns@gmail.com>

---
+++ BFF/MVVM/Services/PayeeViewModelService.cs
@@ -0,0 +1,24 @@
+﻿using BFF.DB;
+using BFF.DB.Dapper.ModelRepositories;
+using BFF.MVVM.Models.Native;
+using BFF.MVVM.ViewModels.ForModels;
+
+namespace BFF.MVVM.Services
+{
+    class PayeeViewModelService : ViewModelServiceBase<Payee, PayeeViewModel>
+    {
+        private readonly PayeeRepository _repository;
+        private readonly IBffOrm _orm;
+
+        public PayeeViewModelService(PayeeRepository repository, IBffOrm orm) : base(repository)
+        {
+            _repository = repository;
+            _orm = orm;
+        }
+
+        protected override PayeeViewModel Create(Payee model)
+            => new PayeeViewModel(model, _orm);
+        public override PayeeViewModel GetNewNonInsertedViewModel()
+            => new PayeeViewModel(_repository.Create(), _orm);
+    }
+}
