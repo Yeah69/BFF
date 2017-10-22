2f17864a20eb2eb053d1835ed35b3404418ea29f
Dima Enns <dieter.enns@gmail.com>
2017-10-22 23:29:11 +0200

    UI-Validation for payees and categories, which checks for accidental null selections.
    Signed-off-by: Dima Enns <dieter.enns@gmail.com>

---
+++ BFF/MVVM/Rules.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using BFF.Helper.Extensions;
+using BFF.MVVM.ViewModels.ForModels;
+
+namespace BFF.MVVM
+{
+    internal static class Rules
+    {
+        public static ValidationRule EmptyPayee =
+            LambdaConverters.Validator.Create<IPayeeViewModel>(
+                e => e.Value != null ? ValidationResult.ValidResult : new ValidationResult(false, "ErrorMessageEmptyPayee".Localize<string>()));
+
+        public static ValidationRule EmptyCategory =
+            LambdaConverters.Validator.Create<ICategoryViewModel>(
+                e => e.Value != null ? ValidationResult.ValidResult : new ValidationResult(false, "ErrorMessageEmptyCategory".Localize<string>()));
+    }
+}
