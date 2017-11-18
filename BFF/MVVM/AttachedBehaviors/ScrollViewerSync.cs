c93662cd9e07ab5b805afa715edaccd787e929aa
Dima Enns <dieter.enns@gmail.com>
2017-11-18 16:16:43 +0100

    Fixed scrolling in BudgetOverview
    Signed-off-by: Dima Enns <dieter.enns@gmail.com>

--- BFF/MVVM/AttachedBehaviors/ScrollViewerSync.cs
+++ BFF/MVVM/AttachedBehaviors/ScrollViewerSync.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading;
@@ -12,20 +15,38 @@ namespace BFF.MVVM.AttachedBehaviors
 {
     public class ScrollViewerSync : Behavior<FrameworkElement>
     {
-        public static readonly DependencyProperty VerticalOffsetProperty = DependencyProperty.Register(
-            nameof(VerticalOffset),
-            typeof(double),
+        private static readonly IDictionary<string, IList<ScrollViewer>> GroupNameToScrollViewers = new Dictionary<string, IList<ScrollViewer>>();
+
+        public static readonly DependencyProperty GroupNameProperty = DependencyProperty.Register(
+            nameof(GroupName),
+            typeof(string),
             typeof(ScrollViewerSync),
-            new PropertyMetadata(default(double), (o, args) =>
+            new PropertyMetadata(default(string), (o, args) =>
             {
-                Thread.Sleep(10);
-                (o as ScrollViewerSync)?._associatedScrollViewer?.ScrollToVerticalOffset((double) args.NewValue);
+                var associatedScrollViewer = (o as ScrollViewerSync)?._associatedScrollViewer;
+                if (args.OldValue != null && 
+                    GroupNameToScrollViewers.ContainsKey((string)args.OldValue) &&
+                    GroupNameToScrollViewers[(string)args.OldValue].Contains(associatedScrollViewer))
+                {
+                    string key = (string) args.OldValue;
+                    GroupNameToScrollViewers[key].Remove(associatedScrollViewer);
+                    if (GroupNameToScrollViewers[key].Count == 0)
+                        GroupNameToScrollViewers.Remove(key);
+                }
+                if (args.NewValue != null)
+                {
+                    string key = (string)args.NewValue;
+                    if (!GroupNameToScrollViewers.ContainsKey(key))
+                        GroupNameToScrollViewers[key] = new List<ScrollViewer>();
+                    if(!GroupNameToScrollViewers[key].Contains(associatedScrollViewer))
+                        GroupNameToScrollViewers[key].Add(associatedScrollViewer);
+                }
             }));
 
-        public double VerticalOffset
+        public string GroupName
         {
-            get => (double)this.GetValue(VerticalOffsetProperty);
-            set => this.SetValue(VerticalOffsetProperty, value);
+            get => (string) GetValue(GroupNameProperty);
+            set => SetValue(GroupNameProperty, value);
         }
 
         private CompositeDisposable _compositeDisposable = new CompositeDisposable();
@@ -40,14 +61,34 @@ namespace BFF.MVVM.AttachedBehaviors
                 _compositeDisposable = new CompositeDisposable();
 
                 _associatedScrollViewer = this.AssociatedObject.GetDescendantByType<ScrollViewer>();
+
+                if (!GroupNameToScrollViewers.ContainsKey(GroupName))
+                    GroupNameToScrollViewers[GroupName] = new List<ScrollViewer>();
+                if (!GroupNameToScrollViewers[GroupName].Contains(_associatedScrollViewer))
+                    GroupNameToScrollViewers[GroupName].Add(_associatedScrollViewer);
+
                 Observable.FromEventPattern<ScrollChangedEventArgs>(_associatedScrollViewer, "ScrollChanged")
                     .ObserveOn(Dispatcher)
-                    .Subscribe(ep => this.VerticalOffset = ep.EventArgs.VerticalOffset)
+                    .Subscribe(ep =>
+                    {
+                        if (GroupName != null
+                            && GroupNameToScrollViewers.ContainsKey(GroupName)
+                            && GroupNameToScrollViewers[GroupName].Any())
+                        {
+                            foreach (var scrollViewer in GroupNameToScrollViewers[GroupName].Except(new []{ _associatedScrollViewer}))
+                            {
+                                scrollViewer?.ScrollToVerticalOffset(_associatedScrollViewer.VerticalOffset);
+                            }
+                        }
+                    })
                     .AddTo(_compositeDisposable);
             }
 
             void OnUnloaded(object sender, RoutedEventArgs e)
             {
+                GroupNameToScrollViewers[GroupName].Remove(_associatedScrollViewer);
+                if (GroupNameToScrollViewers[GroupName].Count == 0)
+                    GroupNameToScrollViewers.Remove(GroupName);
                 _compositeDisposable?.Dispose();
                 _compositeDisposable = null;
                 _associatedScrollViewer = null;
