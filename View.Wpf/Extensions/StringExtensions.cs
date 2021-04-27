f193063ac5dcd78961a2a04e7ccf1b8bf8f08eb8
Dima Enns <dieter.enns@gmail.com>
2021-04-27 18:07:59 +0200

    Fixed localization issue

--- View.Wpf/Extensions/StringExtensions.cs
+++ View.Wpf/Extensions/StringExtensions.cs
@@ -1,14 +1,13 @@
-﻿using System.Reflection;
-using WPFLocalizeExtension.Extensions;
+﻿using WPFLocalizeExtension.Extensions;
 
 namespace BFF.View.Wpf.Extensions
 {
     internal static class StringExtensions
     {
         public static T Localize<T>(this string key) =>
-            LocExtension.GetLocalizedValue<T>(Assembly.GetCallingAssembly().GetName().Name + ":Resources:" + key);
+            LocExtension.GetLocalizedValue<T>("BFF.View.Wpf:Texts:" + key);
 
         public static string Localize(this string key) =>
-            LocExtension.GetLocalizedValue<string>(Assembly.GetCallingAssembly().GetName().Name + ":Resources:" + key);
+            LocExtension.GetLocalizedValue<string>("BFF.View.Wpf:Texts:" + key);
     }
 }
